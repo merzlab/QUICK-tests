c23bff848ea79140cd36e199096a330c3859a6a6
Mystical-Codez <37031914+Saatvik-Aggarwal@users.noreply.github.com>
2021-07-07 13:03:46 -0700

    Update in_to_xyz.sh

--- benchmark_scripts/in_to_xyz.sh
+++ benchmark_scripts/in_to_xyz.sh
@@ -3,14 +3,14 @@
 location=${1:-benchmarkinputs}
 outdir=${2:-xyz}
 
-mkdir -p outdir
+mkdir outdir
 
 for fileraw in $location/*
 do
         file=$(basename $fileraw .in)
         cp $location/$file.in $outdir/$file.xyz
-        number=$(cat foo.c | sed '/^\s*$/d' | wc -l)
-        atomcount="$((number-1))"
+        number=$(cat $outdir/$file.xyz | sed '/^\s*$/d' | wc -l)
+        atomcount="$(($number-1))"
         sed '1 c$atomcount' $outdir/$file.xyz > $outdir/$file.xyz
         sed '2 c$file' $outdir/$file.xyz > $outdir/$file.xyz
 done
