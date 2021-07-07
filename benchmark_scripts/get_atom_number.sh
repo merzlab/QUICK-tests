e473ab1404b6648ed736a107747c7636ba44fefe
Mystical-Codez <37031914+Saatvik-Aggarwal@users.noreply.github.com>
2021-07-07 13:31:14 -0700

    fixed wrong directory bug in get_atom_number

--- benchmark_scripts/get_atom_number.sh
+++ benchmark_scripts/get_atom_number.sh
@@ -8,7 +8,7 @@ outfile=${2:-atomcount.txt}
 for fileraw in $location/*
 do
         file=$(basename $fileraw .in)
-        number=$(cat $outdir/$file.xyz | sed '/^\s*$/d' | wc -l)
+        number=$(cat $location/$file.in | sed '/^\s*$/d' | wc -l)
         atomcount="$(($number-1))"
         echo "$file $atomcount" >> $outfile
 done
