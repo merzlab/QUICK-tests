0f5ba14cf7ce7a0664590f5a8ba824ac569a62c8
nicoleipi <pinicolei@gmail.com>
2021-07-07 19:57:31 -0700

    update empty_line variable

--- Scripts/xyz-maker.sh
+++ Scripts/xyz-maker.sh
@@ -12,19 +12,20 @@ do
 
     empty_line=0
     atom_count=0
-    
+
     rm -rf /tmp/output.txt
     while IFS= read -r line
     do
         line_len=${#line}
         if [ ${line_len} -lt 2 ]; then
-            empty_line=1
+            let "empty_line=empty_line+1"
         fi
 
         if [ ${line_len} -gt 2 ] && [ ${empty_line} -eq 1 ]; then
             let "atom_count=atom_count+1"
             echo "${line}" >> /tmp/output.txt
         fi
+
     done < "${f}"
 
     echo ${atom_count} >> ${xyz_dir}/${name}.xyz
