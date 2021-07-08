9988c1ed07ea5d63fe15aa17c43521ce466672cf
nicoleipi <pinicolei@gmail.com>
2021-07-07 17:28:20 -0700

    scripts v2

--- Scripts/atom-counter.sh
+++ Scripts/atom-counter.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 in_dir=${1}
+output_file=${2}
 
-touch list.txt
+touch ${output_file}
 
 for f in ${in_dir}/*;
 do
     name=`basename ${f} .in`    
     atom_count=`wc -l < ${f}`
     let "atom_count=atom_count-4"
-    echo ${name} ${atom_count} >> atom_count.txt
+    echo ${name} ${atom_count} >> ${output_file}
 done
+
+sort -k 2n ${output_file} > tmp.txt
+mv tmp.txt ${output_file}
