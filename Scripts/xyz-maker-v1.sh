ac933b90fe547324db204194d81a970146a7c441
nicoleipi <pinicolei@gmail.com>
2021-07-07 19:52:01 -0700

    new xyz-maker

--- Scripts/xyz-maker-v1.sh
+++ Scripts/xyz-maker-v1.sh
@@ -10,22 +10,14 @@ do
     name=`basename ${f} .in`    
     touch ${xyz_dir}/${name}.xyz
 
-    empty_line=0
-
     atom_count=0
     rm -rf /tmp/output.txt
     while IFS= read -r line
     do
         line_len=${#line}
-        if [ ${line_len} -lt 5 ]; then
-            empty_line=1
-        fi
-
-        echo ${line} ${empty_line}
-
-        if [ ${line_len} -gt 5 ] && [ ${empty_line} -eq 1 ]; then
-            let "atom_count=atom_count+1"
-            echo "${line}" >> /tmp/output.txt
+        if [ ${line_len} -gt 10 ] && [ ${line_len} -lt 60 ]; then
+        let "atom_count=atom_count+1"
+        echo "${line}" >> /tmp/output.txt
         fi
     done < "${f}"
 
