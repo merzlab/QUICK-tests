8e09c07ff9c7fdc34bd654ae88f48fb72486e797
nicoleipi <pinicolei@gmail.com>
2021-07-15 22:00:18 -0700

    organized xyz files

--- scripts/xyz-divider.sh
+++ scripts/xyz-divider.sh
@@ -9,10 +9,15 @@ out_dir=${2}
 mkdir -p ${out_dir}
 
 #creates subdirectories
+mkdir -p ${out_dir}/ala
+mkdir -p ${out_dir}/water_clusters
 mkdir -p ${out_dir}/tiny
 mkdir -p ${out_dir}/small
 mkdir -p ${out_dir}/medium
 mkdir -p ${out_dir}/large
+
+cp ${xyz_dir}/ala* ${out_dir}/ala
+cp ${xyz_dir}/water* ${out_dir}/water_clusters
 
 for f in ${xyz_dir}/*;
 do
@@ -22,21 +27,22 @@ do
 
     while IFS= read -r line
     do
-        #sets the atom count as the first line of the .xyz file    
+        #sets the atom count as the first line of the .xyz file
         read -r atom_count < ${f}
 
         if [ ${atom_count} -lt 10 ]; then
-            mv ${f} ${out_dir}/tiny
+            cp ${f} ${out_dir}/tiny
 
         elif [ ${atom_count} -ge 10 ] && [ ${atom_count} -lt 25 ]; then
-            mv ${f} ${out_dir}/small
-        
+            cp ${f} ${out_dir}/small
+
         elif [ ${atom_count} -ge 25 ] && [ ${atom_count} -lt 100 ]; then
-            mv ${f} ${out_dir}/medium
+            cp ${f} ${out_dir}/medium
 
         else
-            mve ${f} ${out_dir}/large
+            cp ${f} ${out_dir}/large
         fi
 
     done < "${f}"
 done
+
