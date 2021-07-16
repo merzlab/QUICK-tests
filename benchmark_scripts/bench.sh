c3f00ca1793a008e781d3721bb9cb35e25830970
Saatvik-Aggarwal <hashtagsaatvikagg@gmail.com>
2021-07-15 17:10:37 -0700

    updated scripts

--- benchmark_scripts/bench.sh
+++ benchmark_scripts/bench.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-NCORES='4 8 16 32'
-
 DATADIR=$(pwd)
 
 
 mkdir -p $DATADIR/benchmark/
+
+NCORES='4 8 16 32'
 
 location=${1:-benchmarkinputs}
 
 for fileraw in $location/*
 do
         file=$(basename $fileraw .in)
-        for number in $NOCRES
+        for number in $NCORES
         do
-                cp $location/$file.in $DATADIR/benchmark/$number$file.in
+                cp $location/$file.in $DATADIR/benchmark/${file}_${number}.in
                 sbatch --ntasks $number bm_ind.slurm $number $file
         done
 done
