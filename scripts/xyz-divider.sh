bae06c40ae2701f5afa021e6c6aabff22bf4ca97
nicoleipi <pinicolei@gmail.com>
2021-07-15 21:39:20 -0700

    file divider script

---
+++ scripts/xyz-divider.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#input directory variable
+xyz_dir=${1}
+#output directory variable
+out_dir=${2}
+
+#creates output directory
+mkdir -p ${out_dir}
+
+#creates subdirectories
+mkdir -p ${out_dir}/tiny
+mkdir -p ${out_dir}/small
+mkdir -p ${out_dir}/medium
+mkdir -p ${out_dir}/large
+
+for f in ${xyz_dir}/*;
+do
+
+    #initializing variables
+    atom_count=0
+
+    while IFS= read -r line
+    do
+        #sets the atom count as the first line of the .xyz file    
+        read -r atom_count < ${f}
+
+        if [ ${atom_count} -lt 10 ]; then
+            mv ${f} ${out_dir}/tiny
+
+        elif [ ${atom_count} -ge 10 ] && [ ${atom_count} -lt 25 ]; then
+            mv ${f} ${out_dir}/small
+        
+        elif [ ${atom_count} -ge 25 ] && [ ${atom_count} -lt 100 ]; then
+            mv ${f} ${out_dir}/medium
+
+        else
+            mve ${f} ${out_dir}/large
+        fi
+
+    done < "${f}"
+done
