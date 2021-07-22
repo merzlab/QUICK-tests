6e377f859b1766198e7950ba1a5bd0e572eb6713
Saatvik Aggarwal <saatvik@exp-9-55.expanse.sdsc.edu>
2021-07-22 16:09:15 -0700

    added bm.slurm

--- benchmark_scripts/bench.sh
+++ benchmark_scripts/bench.sh
@@ -6,15 +6,28 @@ DATADIR=$(pwd)
 mkdir -p $DATADIR/benchmark/
 
 NCORES='4 8 16 32'
+NGPUS='1 2 4 8'
 
 location=${1:-benchmarkinputs}
+mode=${2:-cpu}
 
 for fileraw in $location/*
 do
         file=$(basename $fileraw .in)
-        for number in $NCORES
-        do
-                cp $location/$file.in $DATADIR/benchmark/${file}_${number}.in
-                sbatch --ntasks $number bm_ind.slurm $number $file
-        done
+        if [ "$mode" == "gpu" ]; then
+		echo "GPU MODE SELECTED" >> results.txt
+		for num in $NGPUS
+		do
+			cp $location/$file.in $DATADIR/benchmark/${file}_${num}.in
+			nodenum=$(( ($num + 3) / 4 ))
+			sbatch --partition=gpu --account=csd194 --nodes=$nodenum --gpus=$num --ntasks=$num bm.slurm $num $file $mode
+		done
+	else 
+		echo "CPU MODE SELECTED" >> results.txt
+       		for number in $NCORES
+        	do
+			cp $location/$file.in $DATADIR/benchmark/${file}_${number}.in
+			sbatch --partition=compute --account=csd194 --nodes=1 --ntasks $number bm.slurm $number $file $mode
+       		done
+	fi 
 done
