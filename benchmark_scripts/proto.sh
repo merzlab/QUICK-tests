6f0ebf83e06b03dc85d4bcaa3b8ea3f08ab7c869
Saatvik Aggarwal <saatvik@login01.expanse.sdsc.edu>
2021-07-29 15:55:58 -0700

    Working version for CPU benchmarks

--- benchmark_scripts/proto.sh
+++ benchmark_scripts/proto.sh
@@ -5,7 +5,7 @@ DATADIR=$(pwd)
 
 mkdir -p $DATADIR/benchmark/
 
-maxgpus=8
+maxgpus=4
 maxcores=128
 
 location=${1:-inputs}
@@ -15,12 +15,12 @@ for fileraw in $location/*
 do
         file=$(basename $fileraw .in)
         if [ "$mode" == "gpu" ]; then
-		echo "GPU MODE SELECTED" >> results.txt
+		#echo "GPU MODE SELECTED" >> results.txt
 		
 		nodenum=$(( ($maxgpus + 3) / 4 ))
 		sbatch --partition=gpu --account=csd194 --nodes=$nodenum --gpus=$maxgpus --ntasks=$maxgpus --time=02:00:00 bm_handler.slurm $location $file $mode
 	else 
-		echo "CPU MODE SELECTED" >> results.txt
+		#echo "CPU MODE SELECTED" >> results.txt
 	
 		nodenum=$(( ($maxcores + 127) / 128 ))
 		sbatch --partition=compute --account=csd194 --nodes=$nodenum --mem=240G --ntasks-per-node=128 --time=08:00:00 bm_handler.slurm $location $file $mode
