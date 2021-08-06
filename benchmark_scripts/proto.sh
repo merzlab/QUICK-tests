e58288a1230c5013bfffc151a8f6aa2d44c79a03
Mystical-Codez <37031914+Saatvik-Aggarwal@users.noreply.github.com>
2021-08-05 19:32:35 -0700

    added working multi-node gpu code and fixed input file types

--- benchmark_scripts/proto.sh
+++ benchmark_scripts/proto.sh
@@ -5,7 +5,7 @@ DATADIR=$(pwd)
 
 mkdir -p $DATADIR/benchmark/
 
-maxgpus=8
+maxgpus=16
 maxcores=256
 
 location=${1:-inputs}
@@ -13,12 +13,12 @@ mode=${2:-cpu}
 
 for fileraw in $location/*
 do
-        file=$(basename $fileraw .in)
-        if [ "$mode" == "gpu" ]; then
-		#echo "GPU MODE SELECTED" >> results.txt
-		
-		nodenum=$(( ($maxgpus + 3) / 4 ))
-		sbatch --partition=gpu --account=csd194 --nodes=$nodenum --gpus=$maxgpus --ntasks=$maxgpus --time=02:00:00 bm_handler.slurm $location $file $mode
+	file=$(basename $fileraw .in)
+	if [ "$mode" == "gpu" ]; then
+			#echo "GPU MODE SELECTED" >> results.txt
+			sbatch --partition=gpu --account=csd194 --nodes=2 --gpus=8 --ntasks=$maxgpus --time=02:00:00 bm_handler.slurm $location $file $mode two
+			nodenum=$(( ($maxgpus + 3) / 4 ))
+			sbatch --partition=gpu --account=csd194 --nodes=$nodenum --gpus=$maxgpus --ntasks=$maxgpus --time=00:30:00 bm_handler.slurm $location $file $mode four
 	else 
 		#echo "CPU MODE SELECTED" >> results.txt
 	
