efb2d2ae70f254961ad6a38b20114f538228a085
Andreas Goetz <agoetz@sdsc.edu>
2025-06-16 02:39:56 -0700

    Add QUICK-25.03 V100 and A100 Expanse benchmarks

--- benchmarks/QUICK-25.03/A100-Expanse/run-all-single-GPU.sh
+++ benchmarks/QUICK-25.03/A100-Expanse/run-all-single-GPU.sh
@@ -10,8 +10,6 @@ do
     tstart=$(date '+%s')
     $exe $bench.in
     tstop=$(date '+%s')
-    printf "%d" $nt
     echo "$tstart $tstop" | awk '{elapsed=$2-$1; printf " >> Elapsed time = %d seconds = %8.1f minutes\n", elapsed, elapsed/60}'
     mv $bench.out ${bench}-single-GPU.out
-    done
 done
